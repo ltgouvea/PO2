692dc097cf90b017a56971e1656dda084c14e974
ltgouvea <lucas.fim@mailinator.com>
2015-05-20 13:21:44 -0300

    Origem do projeto

---
+++ T1/Trabalho 01 - PO 2 - H. Cicarelli e L. Gouvêa/Trabalho 01 - PO 2 - H. Cicarelli e L. Gouvêa/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Trabalho_01___PO_2___H.Cicarelli_e_L.Gouvêa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ T1/Trabalho 01 - PO 2 - H. Cicarelli e L. Gouvêa/Trabalho 01 - PO 2 - H. Cicarelli e L. Gouvêa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Trabalho_01___PO_2___H.Cicarelli_e_L.Gouvêa
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
