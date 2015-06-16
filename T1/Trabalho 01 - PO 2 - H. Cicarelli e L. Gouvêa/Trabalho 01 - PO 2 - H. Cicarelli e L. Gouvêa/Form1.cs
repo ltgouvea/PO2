c8a0227427dd0b640bc1665f2b68a02a9939f216
ltgouvea <lucas.fim@mailinator.com>
2015-06-16 19:11:45 -0300

    Busca Uniforme em andamento

--- T1/Trabalho 01 - PO 2 - H. Cicarelli e L. Gouvêa/Trabalho 01 - PO 2 - H. Cicarelli e L. Gouvêa/Form1.cs
+++ T1/Trabalho 01 - PO 2 - H. Cicarelli e L. Gouvêa/Trabalho 01 - PO 2 - H. Cicarelli e L. Gouvêa/Form1.cs
@@ -11,9 +11,11 @@ namespace Trabalho_01___PO_2___H.Cicarelli_e_L.Gouvêa
 {
     public partial class Form1 : Form
     {
+        
         int metodo = -1;
+        int k;
         string F;
-        double a, b, l, epsilon, delta;
+        double a, b, l, epsilon, delta, x, fx, u, fu, lambda, flambda, minimo;
         MathParser m = new MathParser();
         public Form1()
         {
@@ -89,6 +91,54 @@ namespace Trabalho_01___PO_2___H.Cicarelli_e_L.Gouvêa
 
         public void BuscaUniforme()
         {
+            double fAntigo, xDescarte;
+            k = 0;
+            m.X = a;
+            fAntigo = m.ValueAsDouble;
+           
+
+            do
+            {
+                a += delta;
+                m.X = a;
+                fx = m.ValueAsDouble;
+
+                if (fx > fAntigo)
+                {
+                    xDescarte = Convert.ToDouble(m.X);
+                    a -= delta;
+                    m.X = a;
+                    delta = delta / 10;                     
+                    while (Convert.ToDouble(m.X) < xDescarte)
+                    {
+                        k++;
+                        fAntigo = m.ValueAsDouble;
+                        a += delta;
+                        m.X = a;
+                        fx = m.ValueAsDouble;
+                        if (fx > fAntigo)
+                        {
+                            minimo = fAntigo;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    k++;
+                    textBoxResultado.AppendText("\r\n K =" + Convert.ToString(k) +"A: "+ Convert.ToString(a) + " minimo: " + Convert.ToString(minimo));
+                }
+                minimo = fAntigo;
+
+            } while (fx < fAntigo);
+
+
+            
+
+
+
+
+
 
         }
         //Fim - Busca Uniforme
@@ -153,11 +203,31 @@ namespace Trabalho_01___PO_2___H.Cicarelli_e_L.Gouvêa
             if (Verifica() == true)
             {
                 //Aplicar o método escolhido
+                switch (metodo)
+                {
+                    case 1:
+                        BuscaUniforme();
+                        break;
+                    case 2:
+                        Bissecao();
+                        break;
+                    case 3:
+                        SecaoAurea();
+                        break;
+                    case 4:
+                        Dicotomica();
+                        break;
+                    case 5:
+                        BuscaFibonacci();
+                        break;
+                    case 6:
+                        Newton();
+                        break;
+                }
             }
             else
             {
                 MessageBox.Show("Erro na entrada de dados");
-               textBoxA.Focus;
             }
         }
         //Fim - Método de Newton
